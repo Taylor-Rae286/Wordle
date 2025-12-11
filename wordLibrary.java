d2da16e9017e31875e6cfe3ba4941c265534c749
Taylor-Rae286 <taylorblair09@gmail.com>
2025-12-11 15:27:33 -0800

    Create wordLibrary.java

---
+++ wordLibrary.java
@@ -0,0 +1,24 @@
+import java.util.Random;
+class wordLibrary{
+    
+    Random random = new Random();
+    
+    char[][] words = {
+        {'K','E','L','L','E','R'},
+        {'J','A','D','E','N'},
+        {'C','O','M','P','U','T','E','R'},
+        {'S','C','I','E','N','C','E'},
+        {'F','A','N','F','I','C','T','I','O','N'},
+        {'E','Y','E','B','A','L','L','S'},
+        {'P','R','E','T','Z','E','L'},
+        {'T','A','Y','L','O','R'},
+        {'Q','U','I','N','N'},
+        {'N','A','T','A','L','I','E'},
+    };
+    wordLibrary(){
+    }
+    
+    char[] getWord(){
+        return words[random.nextInt(10)];
+    }
+}
