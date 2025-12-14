4061a10f76f827ad620b24e4e3fa42b809a273c3
Taylor-Rae286 <taylorblair09@gmail.com>
2025-12-14 10:52:20 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -9,13 +9,28 @@ public class Main
 		hangman George = new hangman();
 		
 		Boolean isRunning = true;
-		int livesLeft = 6;
+		int livesLeft = answer.lives;
 		answer.hideWord();
 		
 		while(isRunning){
 		    George.getHangman(livesLeft);
 		    System.out.println(answer.showGuess());
-		    isRunning = false;
+		    System.out.println("Guess a letter!");
+		    String myGuess = input.next().toUpperCase();
+		    answer.GuessLetter(myGuess.charAt(0));
+		    livesLeft = answer.lives;
+		    if(livesLeft == 0){
+		        isRunning = false;
+		    }
+		    else if(answer.isSolved()){
+		        isRunning = false;
+		    }
+		}
+		if(livesLeft == 0){
+		    George.getHangman(livesLeft);
+		    System.out.println("You lost, better luck next time!");
+		    System.out.print("The word was really ");
+		    answer.showWordToGuess();
 		}
 	}
 }
